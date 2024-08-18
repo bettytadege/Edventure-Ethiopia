634dc8791fb954613914a13b098b1d9c127866a9
bettytadege <bettytadege13@gmail.com>
2024-08-18 21:58:13 +0300

    banner

---
+++ src/components/Banner/Boy.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import boy from "../.././assets/landing.webp";
+function Boy() {
+  return (
+  <>
+  <div className="min-w-[800px] object-cover hidden md:block ">
+          <img src={boy} alt="" />
+        </div>
+  
+  </>
+      
+ 
+  )
+}
+
+export default Boy
